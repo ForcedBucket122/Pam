1f5db65e7d8bf73c4193247c8157f18010adf51b
ForcedBucket122 <dzejkob2112@gmail.com>
2024-03-26 11:18:21 +0100

    notatka i zadanie

--- obiektowe/listy i mapy/untitled/src/Main.java
+++ obiektowe/listy i mapy/untitled/src/Main.java
@@ -1,7 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
-
+        LinkedList<String> lista = new LinkedList<>();
+        lista.add("Polska");
+        lista.add("Polska");
+        lista.add("portugalia");
+        lista.add("Pakistan");
+        for (String l: lista){
+            System.out.println(l);
+        }
+        System.out.println("...................................");
+        HashMap<Integer,String> mapa = new HashMap<>();
+        mapa.put(1,"Polska");
+        mapa.put(2,"Polska");
+        mapa.put(3,"portugalia");
+        mapa.put(4,"Pakistan");
+        System.out.println(mapa.get(2));
+        System.out.println("...................................");
+        for (String m: mapa.values()){
+            System.out.println(m);
+        }
+        System.out.println("...................................");
+        HashSet<String> mapa2 = new HashSet<>();
+        mapa2.add("Polska");
+        mapa2.add("portugalia");
+        mapa2.add("Pakistan");
+        for (String m2: mapa2){
+            System.out.println(m2);
+        }
+        //porownaj z linkedhashset
     }
 }
