90dbfa840254c5980ae03b426c6ec5e113a84511
ForcedBucket122 <dzejkob2112@gmail.com>
2024-04-08 20:11:59 +0200

    dokonczyle mnotatke

--- obiektowe/listy i mapy/untitled/src/Main.java
+++ obiektowe/listy i mapy/untitled/src/Main.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
-        LinkedList<String> lista = new LinkedList<>();
+        LinkedList<String> lista = new LinkedList<>(); //pracuje na dużych zbiorach, metody te same jak w arraylist
         lista.add("Polska");
         lista.add("Polska");
         lista.add("portugalia");
@@ -12,15 +12,15 @@ public class Main {
             System.out.println(l);
         }
         System.out.println("...................................");
-        HashMap<Integer,String> mapa = new HashMap<>();
+        HashMap<Integer,String> mapa = new HashMap<>(); //mapa przyjmuje klucz i wartosc, podobnie jak w tablicach asocjacycjnch, typ generyczny podwojny
         mapa.put(1,"Polska");
-        mapa.put(2,"Polska");
+        mapa.put(2,"Polska"); //Te sane klucze nie beda funkcjonowac
         mapa.put(3,"portugalia");
         mapa.put(4,"Pakistan");
-        System.out.println(mapa.get(2));
+        System.out.println(mapa.get(2)); //get dziala na kluczach nie na indeksach
         System.out.println("...................................");
         for (String m: mapa.values()){
-            System.out.println(m);
+            System.out.println(m); //W tej mapie nie mozemy wyswietlac za pomoca petli obiektowej samych wartosci, dlatego trzeba odwołac się do metody
         }
         System.out.println("...................................");
         HashSet<String> mapa2 = new HashSet<>();
@@ -28,7 +28,7 @@ public class Main {
         mapa2.add("portugalia");
         mapa2.add("Pakistan");
         for (String m2: mapa2){
-            System.out.println(m2);
+            System.out.println(m2); //Po1 nie wyswietla po kolei wartosci, tylkmo losowo, po 2 te same wartosci nie sa wyswietlane
         }
         //porownaj z linkedhashset
     }
