1c2ebc5299b79f2ae624c845a13e3adf2dcd15a8
ForcedBucket122 <dzejkob2112@gmail.com>
2024-10-08 22:40:51 +0200

    cos sie zepsulo ale juz dziala ez?

---
+++ klasa 3/doradca_napoi/app/src/main/java/com/example/doradca_napo_szczypka/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.doradca_napo_szczypka;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.doradca_napo_szczypka.R;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        Spinner marka = (Spinner) findViewById(R.id.marka);
+        String Marc = String.valueOf((marka.getSelectedItem()));
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(Marc+" "+beerType);
+    }
+}
