40be85145838a34664dc813521084e93ed0fc17d
ForcedBucket122 <dzejkob2112@gmail.com>
2024-12-04 12:12:48 +0100

    formularz

---
+++ klasa 3/formularz/app/src/main/java/com/example/formularz/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.formularz;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void ClearAll(View view){
+        EditText login = findViewById(R.id.login);
+        EditText password = findViewById(R.id.password);
+        EditText pesel = findViewById(R.id.pesel);
+        EditText email = findViewById(R.id.email);
+
+        login.setText("");
+        password.setText("");
+        pesel.setText("");
+        email.setText("");
+
+    }
+}
