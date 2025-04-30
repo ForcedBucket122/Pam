a06575088d96c4dc930e3adafb3fa55b847067e1
egzamin <egzamin@wp.pl>
2025-04-30 12:11:21 +0200

    zadanie

--- klasa 3/kalendarzZadanie/app/src/main/java/com/example/zadaniekalendarz/MainActivity.java
+++ klasa 3/kalendarzZadanie/app/src/main/java/com/example/zadaniekalendarz/MainActivity.java
@@ -1,10 +1,11 @@
-package com.example.kalendarz;
+package com.example.zadaniekalendarz;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -29,25 +30,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Calendar c = Calendar.getInstance();
+
+        Calendar c =Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
 
+        EditText data = findViewById(R.id.data);
+
         datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                TextView urodziny_text = findViewById(R.id.urodziny_txt);
-                urodziny_text.setText(String.format("%02d", dayOfMonth)+"."+String.format("%02d", month+1)+"."+year);
+
+                data.setText(String.format("%02d", dayOfMonth)+"."+String.format("%02d", month)+"."+year);
             }
         }, year, month, dayOfMonth);
-        Button button = findViewById(R.id.urodziny);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        data.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 datePickerDialog.show();
             }
         });
+        Button wyslij = findViewById(R.id.wyslij);
+        wyslij.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText imie = findViewById(R.id.imie);
+                EditText nazwisko = findViewById(R.id.nazwisko);
+                String dane = String.valueOf(data.getText()) +
+                        imie.getText() +
+                        nazwisko.getText();
+                TextView wynik = findViewById(R.id.wynik);
+                wynik.setText(dane);
+            }
+        });
     }
-
 }
