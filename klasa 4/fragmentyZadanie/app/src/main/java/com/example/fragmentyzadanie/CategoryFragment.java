ccf649ea3e08d689a0c7db3ba65d3c5391affe95
ForcedBucket <dzejkob2112@gmail.com>
2026-01-28 12:14:46 +0100

    zadanie z fragmentow

---
+++ klasa 4/fragmentyZadanie/app/src/main/java/com/example/fragmentyzadanie/CategoryFragment.java
@@ -0,0 +1,38 @@
+package com.example.fragmentyzadanie;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CategoryFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+
+
+
+        ListView listView = view.findViewById(R.id.list);
+        Bundle bundle = new Bundle();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                bundle.putString("kategoria",String.valueOf(listView.getItemAtPosition(position)) );
+                fragment.setArguments(bundle);
+            }
+        });
+
+        return view;
+    }
+}
